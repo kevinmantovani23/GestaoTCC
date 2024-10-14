7023864ac3f6bfda9342abf1555666e6ef18e283
Kevin Mantovani <kevinmantovani23@gmail.com>
2024-10-14 14:32:56 -0300

    primeiro commit

---
+++ trabalhoTCC/src/main/java/labBd/trabalhoTCC/TrabalhoTccApplication.java
@@ -0,0 +1,13 @@
+package labBd.trabalhoTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoTccApplication.class, args);
+	}
+
+}
---
+++ trabalhoTCC/src/test/java/labBd/trabalhoTCC/TrabalhoTccApplicationTests.java
@@ -0,0 +1,13 @@
+package labBd.trabalhoTCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
